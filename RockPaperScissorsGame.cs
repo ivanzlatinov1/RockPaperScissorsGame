767a6848bf8b45b58f24794834e276d36e7fe24f
Ivan Zlatinov <vani.zlatinov51@gmail.com>
2024-05-14 17:28:43 +0300

    Finished the game.

--- RockPaperScissorsGame.cs
+++ RockPaperScissorsGame.cs
@@ -3,15 +3,17 @@ string Paper = "Paper";
 string Scissors = "Scissors";
 
 int counter = 1;
-int winsCount = 0;
+int playerWinsCount = 0;
+int computerWinsCount = 0;
+Console.ForegroundColor = ConsoleColor.White;
 Console.WriteLine("Welcome! You have 3 games VS AI of Rock Paper Scissors.");
-await Task.Delay(1500);
+await Task.Delay(2000);
 
 while (counter < 4)
 {
-    await Task.Delay(1000);
     Console.ForegroundColor = ConsoleColor.Cyan;
     Console.WriteLine($"Game [{counter}/3]");
+    await Task.Delay(1000);
     Console.ForegroundColor = ConsoleColor.Yellow;
     Console.Write("Choose [r]ock, [p]aper or [s]cissors: ");
 
@@ -68,11 +70,12 @@ while (counter < 4)
     await Task.Delay(1500);
     Console.ForegroundColor = ConsoleColor.Magenta;
     Console.WriteLine($"The computer chose {computerMove}.");
-    //Implementing win/draw/lose logic
 
     await Task.Delay(1000);
     Console.ForegroundColor = ConsoleColor.Yellow;
     Console.WriteLine("Calculating result...");
+
+    //Implementing win/draw/lose logic
 
     if ((playerMove == Rock && computerMove == Scissors) || (playerMove == Paper && computerMove == Rock)
         || playerMove == Scissors && computerMove == Paper)
@@ -80,7 +83,7 @@ while (counter < 4)
         await Task.Delay(4000);
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("You win!");
-        winsCount++;
+        playerWinsCount++;
     }
     else if ((computerMove == Rock && playerMove == Scissors) || (computerMove == Paper && playerMove == Rock)
         || computerMove == Scissors && playerMove == Paper)
@@ -88,6 +91,7 @@ while (counter < 4)
         await Task.Delay(4000);
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("You lose!");
+        computerWinsCount++;
     }
     else
     {
@@ -99,6 +103,19 @@ while (counter < 4)
     counter++;
     Console.WriteLine();
 }
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine($"You won {winsCount} times VS AI.");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+if(playerWinsCount > computerWinsCount)
+{
+    Console.WriteLine($"Victory! The score is {playerWinsCount}:{computerWinsCount} for you.");
+}
+else if(computerWinsCount > playerWinsCount)
+{
+    Console.WriteLine($"Defeat! The score is {computerWinsCount}:{playerWinsCount} for the AI.");
+}
+else
+{
+    Console.WriteLine("The game ended as a draw.");
+    Console.WriteLine($"Score: {playerWinsCount}:{computerWinsCount}");
+}
 Console.ResetColor();
