2aafea757826ee2578953a1f06cf2c84d12714e5
Ivan Zlatinov <vani.zlatinov51@gmail.com>
2024-05-14 17:17:57 +0300

    Modified the game

--- RockPaperScissorsGame.cs
+++ RockPaperScissorsGame.cs
@@ -2,76 +2,103 @@
 string Paper = "Paper";
 string Scissors = "Scissors";
 
-Console.Write("Choose [r]ock, [p]aper or [s]cissors: ");
-
-//Adding playerMove + logic
-
-string playerMove = Console.ReadLine();
-switch (playerMove)
-{
-    case "r":
-    case "rock":
-    case "Rock":
-        playerMove = Rock;
-        break;
-
-    case "p":
-    case "paper":
-    case "Paper":
-        playerMove = Paper;
-        break;
-
-    case "s":
-    case "scissors":
-    case "Scissors":
-        playerMove = Scissors;
-        break;
-
-    default:
-        Console.WriteLine("Invalid input. Try Again...");
-        break;
-}
-
-//Adding ComputerMove + logic
-
-string computerMove = "";
-Random random = new Random();
-int computerRandomNumber = random.Next(1, 4);
-
-switch (computerRandomNumber)
-{
-    case 1:
-        computerMove = Rock;
-        break;
-    case 2:
-        computerMove = Paper;
-        break;
-    case 3:
-        computerMove = Scissors;
-        break;
-}
+int counter = 1;
+int winsCount = 0;
+Console.WriteLine("Welcome! You have 3 games VS AI of Rock Paper Scissors.");
 await Task.Delay(1500);
-Console.WriteLine($"The computer chose {computerMove}.");
 
-//Implementing win/draw/lose logic
+while (counter < 4)
+{
+    await Task.Delay(1000);
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine($"Game [{counter}/3]");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.Write("Choose [r]ock, [p]aper or [s]cissors: ");
 
-await Task.Delay(1000);
-Console.WriteLine("Calculating result...");
+    //Adding playerMove + logic
 
-if ((playerMove == Rock && computerMove == Scissors) || (playerMove == Paper && computerMove == Rock)
-    || playerMove == Scissors && computerMove == Paper)
-{
-    await Task.Delay(4000);
-    Console.WriteLine("You win!");
+    string playerMove = Console.ReadLine();
+    switch (playerMove)
+    {
+        case "r":
+        case "rock":
+        case "Rock":
+        case "ROCK":
+            playerMove = Rock;
+            break;
+
+        case "p":
+        case "paper":
+        case "Paper":
+        case "PAPER":
+            playerMove = Paper;
+            break;
+
+        case "s":
+        case "scissors":
+        case "Scissors":
+        case "SCISSORS":
+            playerMove = Scissors;
+            break;
+
+        default:
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid input. Try Again...");
+            continue;
+    }
+
+    //Adding ComputerMove + logic
+
+    string computerMove = "";
+    Random random = new Random();
+    int computerRandomNumber = random.Next(1, 4);
+
+    switch (computerRandomNumber)
+    {
+        case 1:
+            computerMove = Rock;
+            break;
+        case 2:
+            computerMove = Paper;
+            break;
+        case 3:
+            computerMove = Scissors;
+            break;
+    }
+    await Task.Delay(1500);
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine($"The computer chose {computerMove}.");
+    //Implementing win/draw/lose logic
+
+    await Task.Delay(1000);
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("Calculating result...");
+
+    if ((playerMove == Rock && computerMove == Scissors) || (playerMove == Paper && computerMove == Rock)
+        || playerMove == Scissors && computerMove == Paper)
+    {
+        await Task.Delay(4000);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("You win!");
+        winsCount++;
+    }
+    else if ((computerMove == Rock && playerMove == Scissors) || (computerMove == Paper && playerMove == Rock)
+        || computerMove == Scissors && playerMove == Paper)
+    {
+        await Task.Delay(4000);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("You lose!");
+    }
+    else
+    {
+        await Task.Delay(4000);
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("It's a draw!");
+    }
+    Console.ResetColor();
+    counter++;
+    Console.WriteLine();
 }
-else if ((computerMove == Rock && playerMove == Scissors) || (computerMove == Paper && playerMove == Rock)
-    || computerMove == Scissors && playerMove == Paper)
-{
-    await Task.Delay(4000);
-    Console.WriteLine("You lose!");
-}
-else
-{
-    await Task.Delay(4000);
-    Console.WriteLine("It's a draw!");
-}
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"You won {winsCount} times VS AI.");
+Console.ResetColor();
